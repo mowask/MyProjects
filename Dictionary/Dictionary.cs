7332924bd80dc154b1768238cca4ed5333baf64f
mowask <164919999+mowask@users.noreply.github.com>
2024-08-28 16:34:09 +0300

    Add files via upload

---
+++ Dictionary/Dictionary.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Collections;
+
+namespace Dictionary
+{
+    internal class Dictionary
+    {
+        //string _name {  get; set; }
+        string _dictionaryFilePath { get; set; }
+
+       public  Dictionary<string, List<string>> _dictionary;
+
+
+        public Dictionary(string dictionaryFilePath)
+        {
+            _dictionaryFilePath = dictionaryFilePath;
+            _dictionary = new Dictionary<string, List<string>>();
+        }
+
+        public void LoadDictionary()
+        {
+            if (File.Exists(_dictionaryFilePath))
+            {
+                string[] lines = File.ReadAllLines(_dictionaryFilePath);
+                foreach (var line in lines)
+                {
+                    var parts = line.Split(':');                    
+                    {
+                        var word = parts[0];
+                        var translations = parts[1].Split(',');
+                        _dictionary[word] = new List<string>(translations);
+                        // foreach (var item in _dictionary[word])
+                        //     item.Trim();
+                    }
+                }
+            }
+
+            foreach (var j in _dictionary)
+            {
+                Console.WriteLine($"{j.Key}: {string.Join(", ", j.Value)}");
+            }
+            Console.ReadKey();
+        }
+
+        public void SaveDictionary()
+        {
+            List<string> lines = new List<string>();
+            foreach (var item in _dictionary)
+            {
+                lines.Add($"{item.Key}:{string.Join(",", item.Value)}");
+            }
+            File.WriteAllLines(_dictionaryFilePath, lines);
+        }
+
+        public void FindTranslation()
+        {
+            Console.Write("Введите слово для поиска: ");
+            var word = Console.ReadLine();
+            if (_dictionary.ContainsKey(word))
+            {
+                Console.WriteLine("Перевод:");
+                foreach (var translation in _dictionary[word])
+                {
+                    Console.WriteLine($"- {translation}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Слово не найдено.");
+            }
+        }
+
+        public void AddWord()
+        {
+            Console.Write("Введите слово: ");
+            var word = Console.ReadLine();
+            Console.Write("Введите перевод (через запятую): ");
+            var translations = Console.ReadLine().Split(',');
+
+            if (!_dictionary.ContainsKey(word))
+            {
+                _dictionary[word] = new List<string>(translations);
+            }
+            else
+            {
+                _dictionary[word].AddRange(translations);
+            }
+        }
+
+        public void ReplaceWordOrTranslation()
+        {
+            Console.Write("Введите слово для замены: ");
+            var word = Console.ReadLine();
+
+            if (_dictionary.ContainsKey(word))
+            {
+                Console.Write("Введите новый перевод (через запятую): ");
+                var translations = Console.ReadLine().Split(',');
+                _dictionary[word] = new List<string>(translations);
+                Console.WriteLine("Первод успешно изменен");
+            }
+            else
+            {
+                Console.WriteLine("Слово не найдено.");
+            }
+        }
+
+        public void DeleteWord()
+        {
+            Console.Write("Введите слово для удаления: ");
+            var word = Console.ReadLine();
+
+            if (_dictionary.ContainsKey(word))
+            {
+                _dictionary.Remove(word);
+                Console.WriteLine("Слово удалено.");
+            }
+            else
+            {
+                Console.WriteLine("Слово не найдено.");
+            }
+        }
+
+        public void ExportSelectedWord()
+        {
+            Console.Write("Введите слово для экспорта: ");
+            var exportWord = Console.ReadLine();
+
+            if (_dictionary.ContainsKey(exportWord))
+            {
+                Console.Write("Введите имя файла для экспорта: ");
+                var exportPath = Console.ReadLine();
+
+                List<string> exportLines = new List<string>();
+                exportLines.Add($"{exportWord}:{string.Join(",", _dictionary[exportWord])}");
+
+                File.WriteAllLines(exportPath, exportLines);
+                Console.WriteLine($"Слово '{exportWord}' экспортировано в файл '{exportPath}'.");
+            }
+            else
+            {
+                Console.WriteLine($"Слово '{exportWord}' не найдено в словаре.");
+            }
+        }
+
+    }
+}
---
+++ Dictionary/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dictionary
+{
+    internal class Program
+    {        
+        static void Main(string[] args)
+        {
+            Dictionary dictionary;
+           // Dictionary englishRussian = new Dictionary("Edictionary.txt");
+           // Dictionary russianEnglish = new Dictionary("Rdictionary.txt");
+
+            Console.WriteLine("Выбери словарь: \n1. Англо-Русский \n2. Русско-английский \nДля выхода - нахмите любую клавишу" );
+
+            ConsoleKey key = Console.ReadKey().Key;
+
+            if ( key == ConsoleKey.D1 )
+            {
+                dictionary = new Dictionary("Edictionary.txt");
+                dictionary.LoadDictionary();
+            }
+            else if ( key == ConsoleKey.D2 )
+            {
+                dictionary = new Dictionary("Rdictionary.txt");
+                dictionary.LoadDictionary();
+            } 
+            else 
+            {
+                Console.WriteLine("\nБудь внимательней в следующий раз");
+                Console.ReadKey();
+                return;
+                
+            }
+
+            //int choice = Console.ReadKey().KeyChar;
+            //while (true)
+            //{
+            //    Console.WriteLine("\nНеверный выбор. Будь внимательней в следующий раз.");
+            //if (choice == 1)
+            //    englishRussian.LoadDictionary();
+            //else if (choice == 2)
+            //    russianEnglish.LoadDictionary();
+            //else return;
+            //}
+
+
+            //foreach (string i in russianEnglish._dictionary.Keys)
+            //{
+            //    Console.WriteLine(i);
+            //}                        
+            //Console.ReadKey();
+
+
+
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("Меню:");
+                Console.WriteLine("1. Найти перевод слова");
+                Console.WriteLine("2. Добавить слово и перевод");
+                Console.WriteLine("3. Заменить слово или перевод");
+                Console.WriteLine("4. Удалить слово");
+                Console.WriteLine("5. Экспортировать слово в файл");
+                Console.WriteLine("6. Выйти");
+                Console.Write("Выберите пункт меню: ");
+
+                switch (Console.ReadLine())
+                {
+                    case "1":
+                        dictionary.FindTranslation();
+                       
+                        break;
+                    case "2":
+                        dictionary.AddWord();                       
+                        break;
+                    case "3":
+                        dictionary.ReplaceWordOrTranslation();                        
+                        break;
+                    case "4":
+                        dictionary.DeleteWord();                        
+                        break;
+                    case "5":
+                        dictionary.ExportSelectedWord();                       
+                        break;
+                    case "6":
+                        dictionary.SaveDictionary();                        
+                        return;
+                    default:
+                        Console.WriteLine("Неверный выбор. Попробуйте снова.");
+                        break;
+                }
+                Console.WriteLine("Нажмите любую клавишу для продолжения...");
+                Console.ReadKey();
+            }
+        }
+    }
+}
